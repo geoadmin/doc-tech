cb331ae2b616137a4577d8ad5ac5ca14414c0972
Pedro Soares <pedro.soares@camptocamp.com>
2025-09-03 09:22:35 +0200

    improve fetch function

--- scripts/available-layers.data.ts
+++ scripts/available-layers.data.ts
@@ -1,5 +1,10 @@
 export default {
   async load() {
-    return (await fetch("https://api3.geo.admin.ch/rest/services/api/MapServer/layersConfig?lang=en")).json();
+    try {
+      return (await fetch("https://api3.geo.admin.ch/rest/services/api/MapServer/layersConfig?lang=en")).json();
+    } catch (error) {
+      console.error("Error loading layers:", error);
+      throw error;
+    }
   }
 }
