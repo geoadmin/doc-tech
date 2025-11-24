dc2d1901d9d767a36b64da143ee9622cf71778f9
Pedro Soares <pedro.soares@camptocamp.com>
2025-11-24 15:45:39 +0100

    format

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -44,9 +44,8 @@ export default {
                     initMermaid()
                 }
             )
-
         })
-        
+
         nextTick(() => {
             // Initialize Mermaid after DOM is ready
             initMermaid()
